3d05c411879e9698b38ec2f9ecf6de5f8c79c58a
Eric Foucault <efoucault@gmail.com>
2018-09-11 19:23:16 +0200

    ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,7 @@
 
 let circle = document.querySelector(".circle");
 circle.addEventListener("click", (event) => {
-  const audio = document.querySelector(".test");
+  let audio = document.querySelector(".test");
   audio.currentTime = 0;
   audio.play();
 });
