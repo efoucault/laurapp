a341a914da02e7150bf8fab1de4b211d8770a5f7
Eric Foucault <efoucault@gmail.com>
2018-09-11 23:59:52 +0200

    ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,4 @@
 //= require rails-ujs
 //= require_tree .
 
-let circle = document.querySelector(".circle");
-circle.addEventListener("click", (event) => {
-  let audio = document.querySelector(".test");
-  audio.currentTime = 0;
-  audio.play();
-});
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,8 @@
 import "bootstrap";
+
+let circle = document.querySelector(".circle");
+circle.addEventListener("click", (event) => {
+  let audio = document.querySelector(".test");
+  audio.currentTime = 0;
+  audio.play();
+});
