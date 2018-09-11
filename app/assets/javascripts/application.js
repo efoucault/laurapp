55d43a0a073acab81387e4beef1c44348f811473
Eric Foucault <efoucault@gmail.com>
2018-09-11 18:06:24 +0200

    ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,12 @@
 //= require rails-ujs
 //= require_tree .
+
+const circle = document.querySelector(".circle");
+if (circle) {
+  circle.addEventListener("click", (event) => {
+    const audio = document.querySelector('.test');
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+  });
+}
