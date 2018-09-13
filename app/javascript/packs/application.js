0f4ce998fbf69b7e0ff237b0f10a85d6aa4a276b
Eric Foucault <efoucault@gmail.com>
2018-09-13 18:15:32 +0200

    ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,32 @@
 import "bootstrap";
 
+const ids = [1, 2, 3, 4, 5, 6]
+const laura = document.querySelector(".animated-gif");
 const circle = document.querySelector(".circle");
+const songs = document.querySelectorAll(".test");
+const postits = document.querySelectorAll(".postit");
+const gifs = document.querySelectorAll(".gif");
+
 circle.addEventListener("click", (event) => {
-  const songs = document.querySelectorAll(".test");
-  const gifs = document.querySelectorAll(".animated-gif");
-  gifs.forEach((gif) => {
-    gif.classList.add("hidden");
-  });
-  gifs[Math.floor(Math.random()*gifs.length)].classList.remove("hidden");
-  var song = songs[Math.floor(Math.random()*songs.length)];
-  song.currentTime = 0;
-  song.play();
+  laura.classList.remove("hidden");
+  circle.classList.add("hidden");
+  var angle=0;
+  setInterval(function(){
+    laura.style.transform="rotateZ("+ angle++ +"deg)";
+  }, 10);
+  setTimeout(function () {
+    laura.classList.add("hidden");
+    const id = ids[Math.floor(Math.random()*ids.length)];
+    console.log(id);
+    const text = document.querySelector(`h1[data-id="${id}"]`);
+    const gif = gifs[Math.floor(Math.random()*gifs.length)];
+    gif.classList.remove("hidden");
+    text.classList.remove("hidden");
+    const song = document.querySelector(`audio[data-id="${id}"]`);
+    song.currentTime = 0;
+    song.play();
+    song.onended = function() {
+      location.reload();
+    };
+  }, 5000);
 });
