243dfd5e5baf1210f292b5646ce19778ec44eca2
Eric Foucault <efoucault@gmail.com>
2018-09-13 22:10:28 +0200

    roulette

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,13 @@ import "bootstrap";
 const ids = [1, 2, 3, 4, 5, 6]
 const laura = document.querySelector(".animated-gif");
 const circle = document.querySelector(".circle");
-const songs = document.querySelectorAll(".test");
-const postits = document.querySelectorAll(".postit");
+const roulette = document.querySelector(".roulette");
 const gifs = document.querySelectorAll(".gif");
 
 circle.addEventListener("click", (event) => {
   laura.classList.remove("hidden");
   circle.classList.add("hidden");
+  roulette.play();
   var angle=0;
   setInterval(function(){
     laura.style.transform="rotateZ("+ angle++ +"deg)";
@@ -17,7 +17,6 @@ circle.addEventListener("click", (event) => {
   setTimeout(function () {
     laura.classList.add("hidden");
     const id = ids[Math.floor(Math.random()*ids.length)];
-    console.log(id);
     const text = document.querySelector(`h1[data-id="${id}"]`);
     const gif = gifs[Math.floor(Math.random()*gifs.length)];
     gif.classList.remove("hidden");
@@ -28,5 +27,5 @@ circle.addEventListener("click", (event) => {
     song.onended = function() {
       location.reload();
     };
-  }, 5000);
+  }, 3000);
 });
